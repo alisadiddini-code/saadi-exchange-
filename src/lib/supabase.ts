33a98c6cb4659362ad955b0a57a40d266f85edb2
alisadiddini-code <alisadiddini@gmail.com>
2026-04-27 04:37:23 +0500

    hardcode correct supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xwbtsshqhqnsbqbreto.supabase.co';
+const supabaseUrl = 'https://xwbtsshqhqnsgbqbreto.supabase.co';
 const supabaseKey = 'sb_publishable_L8Y0PhVsDN7doVfebq_ZaQ_x3HSeYoy';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
