74bb46d980cabed720231a18385bf706c3060df8
alisadiddini-code <alisadiddini@gmail.com>
2026-05-20 20:18:29 +0500

    fix supabase api key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://xwbtsshqhqnsgbqbreto.supabase.co";
-const supabaseKey = "sb_publishable_L8Y0PhVsDN7doVfebq_ZaQ_x3HSeYoy";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3YnRzc2hxaHFuc2dicWJyZXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5ODg1NTAsImV4cCI6MjA5MjU2NDU1MH0.VbLk2_MXgMMl3iWt9u4PVvmfQd-bYqe2xYCwAJm4-UI";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
